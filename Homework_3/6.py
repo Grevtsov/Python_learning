f828911dae8066d8e9b96234f5f3d49fb35b6135
Grevtsov <Fridrih-Wert@yandex.ru>
2021-04-09 19:36:27 +0300

    Homework_3
    Домашнее задание 3. Нумерация файлов соответствует номеру домашнего задания. В последних заданиях не успел сделать описание функций.

---
+++ Homework_3/6.py
@@ -0,0 +1,10 @@
+def int_func(word):
+    return word.capitalize()
+
+def ext_func(*args):
+    words = input('Введите слова через пробел: ').split()
+    for word in words:
+        print(int_func(word), end=' ')
+    print()
+
+ext_func()
